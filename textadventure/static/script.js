d02e8d301ab3f304b0064bb019c68ff249af8ac0
AnishBashyal <anishdbashyal@gmail.com>
2023-05-14 23:18:23 +0545

    Ignore

--- textadventure/static/script.js
+++
@@ -1,6 +0,0 @@
-
-function handleRadioButtonClick(radioButton) {
-    var formOptionSection = document.getElementById('formOptionSection');
-    formOptionSection.style.display = (radioButton.value === 'none') ? 'block' : 'none';
-
-}
